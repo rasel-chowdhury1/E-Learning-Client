46452a0865e33c63557469de6d4ce24ee26e7a09
Rasel Chowdhury <rasel.chowdhury0001@gmail.com>
2024-11-14 10:31:42 +0600

    footer ui done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Banner from "./components/Banner";
 import Banner2 from "./components/Banner2";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Services from "./components/Services";
 import Subscribe from "./components/Subscibe";
@@ -13,6 +14,7 @@ function App() {
         <Banner/>
         <Subscribe/>
         <Banner2/>
+        <Footer/>
     </div>
   )
 }
