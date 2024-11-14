0b214508a834619868bb09cc1820fadee08bf7f2
Rasel Chowdhury <rasel.chowdhury0001@gmail.com>
2024-11-14 10:24:19 +0600

    banner section ui done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import Banner from "./components/Banner";
 import Hero from "./components/Hero";
+import Services from "./components/Services";
 
 
 function App() {
   return (
     <div  className="overflow-x-hidden bg-white text-dark">
         <Hero/>
+        <Services/>
+        <Banner/>
     </div>
   )
 }
